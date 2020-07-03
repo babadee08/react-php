a2f4866fd9c5d74d8d4fbe96139d1a857383da8a
Damilare <babadee08@gmail.com>
2020-07-03 05:58:18 +0100

    Using Query string

---
+++ http/posts.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    [
+        'title' => 'post 1',
+        'body' => 'This is a very simple body',
+        'tags' => ['php', 'solid']
+    ],
+    [
+        'title' => 'post 2',
+        'body' => 'This is a very simple body',
+        'tags' => ['php', 'api']
+    ],
+    [
+        'title' => 'post 3',
+        'body' => 'This is a very simple body',
+        'tags' => ['js', 'async']
+    ],
+    [
+        'title' => 'post 4',
+        'body' => 'This is a very simple body',
+        'tags' => ['mysql', 'database']
+    ],
+    [
+        'title' => 'post 5',
+        'body' => 'This is a very simple body',
+        'tags' => ['solid', 'oop']
+    ],
+    [
+        'title' => 'post 6',
+        'body' => 'This is a very simple body',
+        'tags' => ['js', 'angular']
+    ],
+];
--- http/server.php
+++ http/server.php
@@ -9,11 +9,29 @@ require "vendor/autoload.php";
 
 $loop = Factory::create();
 
-$server = new \React\Http\Server(function (ServerRequestInterface $request) {
+$posts = require "posts.php";
+
+$server = new \React\Http\Server(function (ServerRequestInterface $request) use ($posts) {
+    $params = $request->getQueryParams();
+    $tag = $params['tag'] ?? null;
+
+    $filteredPosts = array_filter($posts, function ($post) use ($tag) {
+        if ($tag) {
+            return in_array($tag, $post['tags']);
+        }
+        return true;
+    });
+
+
+    $page = $params['page'] ?? 1;
+    $filteredPosts = array_chunk($filteredPosts, 3);
+    $filteredPosts = $filteredPosts[$page - 1] ?? [];
+
     echo 'Request to ' . $request->getUri() . PHP_EOL;
-    $body = "The method of the request is: " . $request->getMethod();
+    /*$body = "The method of the request is: " . $request->getMethod();
     $body .= "The requested path is: " . $request->getUri()->getPath();
-    return new Response(200, ['Content-Type' => 'text/plain'], $body);
+    return new Response(200, ['Content-Type' => 'text/plain'], $body);*/
+    return new Response(200, ['Content-Type' => 'application/json'], json_encode($filteredPosts));
 });
 
 // $socket = new \React\Socket\Server('127.0.0.1:8000', $loop);
